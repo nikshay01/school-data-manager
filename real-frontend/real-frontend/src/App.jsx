326a5d3a230c05f6e6ae9b605c130046134d1d87
unknown <vestige5586@gmail.com>
2025-11-13 23:21:52 -0800

    started building react frontend and created blob bg

---
+++ real-frontend/real-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Login from "./componants/login";
+import Bg from "./componants/bg"
+const App = () => {
+  return (
+    <div >
+      <Login/>
+      
+    </div>
+    
+  );
+};
+
+export default App;
