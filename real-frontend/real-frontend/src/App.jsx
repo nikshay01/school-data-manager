506a397cd75b5e6dfa35e70abf957856aeab937b
unknown <vestige5586@gmail.com>
2025-11-17 07:19:10 +0530

    frontend

---
+++ real-frontend/real-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Login from "./componants/login";
+import Bg from "./componants/bg"
+const App = () => {
+  return (
+    <div >
+      <Login/>
+      <Bg/>
+    </div>
+    
+  );
+};
+
+export default App;
--- real-frontend/src/componants/login.jsx
+++ real-frontend/src/componants/login.jsx
@@ -4,7 +4,13 @@ function login() {
   return (
     <div className='login-body'>
         <div className='login-card'>
-
+          <h1>LOGIN</h1>
+            <div className='input-fields'>
+              <label htmlFor="USERNAME" className='label'>USERNAME</label>
+                      <input type="text" className='input' id='USERNAME' />
+                      <input type="text" className='input' />
+                      <input type="text" className='input' />
+            </div>
         </div>
     </div>
   )
