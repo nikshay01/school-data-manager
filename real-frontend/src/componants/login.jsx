2643b0f5f046b16699394f2b241304233e6ac066
nikshay <vestige5586@gmail.com>
2025-11-18 07:10:47 +0530

    vite tweaks

--- real-frontend/src/componants/login.jsx
+++ real-frontend/src/componants/login.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "../App.css";
 function login() {
   return (
-    <div className='login-body'>
-        <div className='login-card'>
-          <h1>LOGIN</h1>
-            <div className='input-fields'>
-              <label htmlFor="USERNAME" className='label'>USERNAME</label>
-                      <input type="text" className='input' id='USERNAME' />
-                      <input type="text" className='input' />
-                      <input type="text" className='input' />
-            </div>
+    <div className="login-body">
+      <div className="login-card">
+        <h1>LOGIN</h1>
+        <div className="input-fields">
+          <label htmlFor="USERNAME" className="label">
+            USERNAME
+          </label>
+            la
+          <input type="text" className="input" id="USERNAME" />
+          <input type="text" className="input" />
+          <input type="text" className="input" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default login
+export default login;
